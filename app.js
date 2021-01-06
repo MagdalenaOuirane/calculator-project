59fa3512602051bc39dc54e76a83d2df29b4af12
Magdalena <magdalenaouirane@gmail.com>
2021-01-06 22:52:19 +0100

    add functionality for calculator program

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
+const allButtons = document.querySelectorAll(".btn");
 
+const input = document.querySelector(".screen");
+
+const equalBtn = document.querySelector(".btn-equal");
+
+const clearBtn = document.querySelector(".btn-clear");
+
+allButtons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+      console.log(btn);
+    const value = e.target.dataset.num;
+    input.value += value;
+    console.log(input);
+  });
+});
+
+equalBtn.addEventListener("click", (e) => {
+  if (input.value === "") {
+    input.value = "Enter a value";
+  } else {
+    let answer = eval(input.value);
+    input.value = answer;
+  }
+});
+
+clearBtn.addEventListener("click", (e) => {
+  input.value = "";
+});
